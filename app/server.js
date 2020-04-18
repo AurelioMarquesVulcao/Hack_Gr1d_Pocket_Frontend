271189fb1d434fe6e1d41cb2d3547bd7be6b2f31
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2020-04-17 21:11:31 -0300

    fix bug

--- app/server.js
+++ app/server.js
@@ -12,9 +12,9 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sen  dFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
 
   console.log('Console listening!')
