965bf3f3ef4632328c970f1a9010387cdd9e27c1
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2020-04-18 10:54:07 -0300

    modificações 01

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main/main.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
@@ -10,6 +11,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [],
+  bootstrap:[AppComponent]
 })
 export class AppRoutingModule { }
